f90c12336539ae4d90649a268b1cf418d5a27a26
sontn2411 <sontn2411@gmail.com>
2025-05-07 19:09:24 +0700

    change port

---
+++ src/lib/data.ts
@@ -0,0 +1,17 @@
+export const getSettings = async () => {
+  return {
+    Logo: '/assets/logo.svg',
+    Time: {
+      Store: '09:00 AM - 06:00 PM',
+      Office: {
+        MondayToFriday: '08 AM - 04 PM',
+        Saturday: '09 AM - 12 AM',
+      },
+    },
+    Socials: {
+      Facebook: 'https://facebook.com/yourpage',
+      Twitter: 'https://twitter.com/yourpage',
+      Instagram: 'https://instagram.com/yourpage',
+    },
+  }
+}
