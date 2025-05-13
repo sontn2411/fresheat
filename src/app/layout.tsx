56c5aa5384253dc74b2806f97b028ea01b3f9fc6
son.tran <son.tran@sweetsoft.vn>
2025-05-13 09:11:54 +0700

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,37 @@ import '../styles/index.scss'
 
 export const metadata: Metadata = {
   title: 'Fresheat food & Restaurant',
+  description:
+    'Nhà hàng phục vụ món ăn tươi ngon mỗi ngày. Đặt bàn nhanh chóng.',
   icons: '/favicon.png',
+  keywords: ['nhà hàng', 'ăn ngon', 'Fresheat', 'đặt bàn'],
+  openGraph: {
+    title: 'Fresheat food & Restaurant',
+    description: 'Món ăn ngon - Phục vụ tận tâm',
+    url: 'http://3.1.29.186:3000/',
+    siteName: 'Fresheat',
+    images: [
+      {
+        url: '/images/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Fresheat Banner',
+      },
+    ],
+    locale: 'vi_VN',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  verification: {
+    google: 'LFBqZiypA32lruY9kf96jLbQtiGXRVv84rQml6b4K-Y',
+  },
 }
 
 export default function RootLayout({
@@ -14,10 +44,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <meta
-        name='google-site-verification'
-        content='LFBqZiypA32lruY9kf96jLbQtiGXRVv84rQml6b4K-Y'
-      />
       <body>{children}</body>
     </html>
   )
